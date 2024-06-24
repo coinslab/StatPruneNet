71668531e1321cef71d734596dfbfc7635fb8827
Adrian Rodriguez <axrod2718@gmail.com>
2024-06-24 11:05:30 -0500

    Generalized logistic regression

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -2,21 +2,11 @@ import torch.nn as nn
 import torch
 
 class LogisticRegression(nn.Module):
-    def __init__(self):
+    def __init__(self, input_layer, output_layer):
         super(LogisticRegression, self).__init__()
-        self.fc1 = nn.Linear(30, 1)
+        self.fc1 = nn.Linear(input_layer, output_layer)
+
 
     def forward(self, input):
-        y_hat = torch.sigmoid(self.fc1(input))
-        return y_hat
-
-# Simple neural network with 784 neurons in the input layer, 0 hidden layers,
-# and 1 output layer with sigmoid
-class MNISTLogisticRegression(nn.Module):
-    def __init__(self):
-        super(MNISTLogisticRegression, self).__init__()
-        self.fc1 = nn.Linear(784, 1)
-
-    def forward(self, input):
-        y_hat = torch.sigmoid(self.fc1(input))
+        y_hat = self.fc1(input)
         return y_hat

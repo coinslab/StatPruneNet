78ed0f52c395e08b047f47e36f9091eac0737d52
Adrian Rodriguez <axrod2718@gmail.com>
2024-07-02 18:58:48 -0500

    Additions

---
+++ run/kfold.py
@@ -0,0 +1,23 @@
+import torchvision
+from torchvision.transforms.transforms import ToTensor
+from torch.utils.data import Subset, Dataset
+import torch
+
+def KFold(dataset, k=10, random_state=None):
+    if random_state is not None:
+        torch.manual_seed(random_state)
+    length = len(dataset)
+    indices = torch.randperm(length).tolist()
+
+    fold_sizes = [length // k] * k
+    for i in range(length % k):
+        fold_sizes[i] += 1
+
+    folds = []
+    current = 0
+    for fold_size in fold_sizes:
+        start, stop = current, current + fold_size
+        folds.append(Subset(dataset, indices[start:stop]))
+        current = stop
+
+    return folds

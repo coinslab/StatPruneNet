3bfdfca352da62401b1eb60b8874cd5646dc16e9
Adrian Rodriguez <axrod2718@gmail.com>
2024-07-11 01:51:40 -0500

    Added gradient computations

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-from torchvision.transforms.transforms import ToTensor
-from models.softmax_regression import SoftmaxRegression
+from torchvision.transforms import transforms
+from models.mlp import MLP
 import torch.optim as optim
 import torch.nn as nn
 from run.evaluate import Evaluate
 import torchvision
 
-if __name__ == '__main__':
-    train_dataset = torchvision.datasets.FashionMNIST(root='./data', train=True, transform=ToTensor(), download=True)
-    test_dataset = torchvision.datasets.FashionMNIST(root='./data', train=False, transform=ToTensor(), download=True)
 
-    model = SoftmaxRegression(784, 10)
+if __name__ == '__main__':
+    transform = transforms.Compose([
+        #transforms.RandomHorizontalFlip(),
+        #transforms.RandomCrop(28, padding=4),
+        #transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    train_dataset = torchvision.datasets.FashionMNIST(root='./data', train=True, transform=transform, download=False)
+    test_dataset = torchvision.datasets.FashionMNIST(root='./data', train=False, transform=transform, download=False)
+
+    model = MLP(784, 10)
     learning_rate = 1
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.LBFGS(model.parameters(), lr=learning_rate)
-    epochs = 5
+    optimizer = optim.LBFGS(model.parameters(),
+                            lr=learning_rate,
+                            #max_iter=200,
+                            #max_eval=250,
+                            #tolerance_grad=1e-20,
+                            #tolerance_change=1e-27,
+                            #history_size=50,
+                            line_search_fn='strong_wolfe')
+
+    epochs = 500
 
     Evaluate(model=model,
             train_dataset=train_dataset,
--- run/evaluate.py
+++ run/evaluate.py
@@ -30,9 +30,73 @@ class Evaluate():
 
         self.train()
 
+    def compute_grad(self, x, y):
+        x = x.unsqueeze(0)
+        y = y.unsqueeze(0)
+
+        prediction = self.model(x)
+        loss = self.criterion(prediction, y)
+
+        gradients = list(torch.autograd.grad(loss, list(self.model.parameters())))
+        gradients = torch.cat([g.view(-1) for g in gradients])
+
+        return gradients
+
+    def compute_G(self, X, Y):
+        G = torch.stack([self.compute_grad(X[i], Y[i]) for i in range(len(self.train_dataset))])
+
+        return G
+
+    def GRADMAX(self, G):
+        average = G.mean(dim=0)
+        absolute = average.abs()
+        gradmax = absolute.max()
+
+        return gradmax.item()
+
     def train(self):
         self.model.to(self.device)
+        self.model.train()
 
+        trainloader = DataLoader(self.train_dataset, batch_size=len(self.train_dataset), shuffle=False, num_workers=0)
+
+        for epoch in range(self.epochs):
+            loss = 0.0
+            for x, y in trainloader:
+                x, y = x.to(self.device), y.to(self.device)
+
+                def closure():
+                    self.optimizer.zero_grad()
+                    y_hat = self.model(x)
+                    loss = self.criterion(y_hat, y)
+
+                    #params = list(self.model.parameters())
+                    params = torch.cat([p.view(-1) for p in self.model.parameters()])
+
+                    l2_lambda = 0.001 / (2 * len(self.train_dataset))
+                    l2_norm = params.pow(2.0).sum() #sum(p.pow(2.0).sum() for p in self.model.parameters())
+                    loss += l2_lambda * l2_norm
+
+                    log_cosh_lambda = 0.001 / len(self.train_dataset)
+                    log_cosh_norm = torch.log(torch.cosh(2.3099 * params)).sum() #sum(torch.log(torch.cosh(2.3099 * p)).sum() for p in self.model.parameters())
+                    loss += log_cosh_lambda * log_cosh_norm
+
+                    loss.backward()
+
+                    return loss
+
+                loss += self.optimizer.step(closure)
+
+                G = self.compute_G(x, y)
+                gradmax = self.GRADMAX(G)
+
+                if (gradmax < 0.007):
+                    print("GRADMAX value achieved")
+                    return
+
+            print(f"\n\tEpoch = {epoch + 1}\tTraining loss = {loss:.2f}")
+
+        '''
         kfolds = KFold(self.train_dataset)
         k = len(kfolds)
 
@@ -54,11 +118,11 @@ class Evaluate():
             train_folds = kfolds[:fold] + kfolds[fold + 1:]
             trainset = ConcatDataset(train_folds)
 
+            # Using LBFGS optimizer, so the batch_size is the entire dataset (FashionMNIST)
             val_loader = DataLoader(val_fold, batch_size=len(val_fold), shuffle=False, num_workers=2)
             trainloader = DataLoader(trainset, batch_size=len(trainset), shuffle=True, num_workers=2)
 
             for epoch in range(self.epochs):
-                self.model.train()
                 for x, y in trainloader:
                     x, y = x.to(self.device), y.to(self.device)
 
@@ -66,13 +130,22 @@ class Evaluate():
                         self.optimizer.zero_grad()
                         y_hat = self.model(x)
                         loss = self.criterion(y_hat, y)
+
+                        # l2_lambda = 0.5 / (2 * len(self.train_dataset))
+                        # l2_norm = sum(i.pow(2.0).sum() for i in self.model.parameters())
+                        # loss += l2_lambda * l2_norm
+
+                        # l1_lambda = 0.5 / len(self.train_dataset)
+                        # l1_norm = sum(i.abs().sum() for i in self.model.parameters())
+                        # loss += l1_lambda * l1_norm
+
                         loss.backward()
 
                         return loss
 
                     loss = self.optimizer.step(closure)
 
-                    print(f"\n\tEpoch = {epoch + 1}\tTraining loss = {loss.item():.2f}")
+                print(f"\n\tEpoch = {epoch + 1}\tTraining loss = {loss.item():.2f}")
 
             train_accuracy, train_precision, train_recall = metrics(self.model, self.device, trainloader)
             train_accuracies[fold] = train_accuracy
@@ -112,3 +185,4 @@ class Evaluate():
         print(f"Accuracy = {val_accuracy:.2f}%")
         print(f"Precision = {val_precision:.2f}%")
         print(f"recall = {val_recall:.2f}%")
+        '''
